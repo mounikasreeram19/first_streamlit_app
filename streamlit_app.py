9eb3bbdfe3454f46fa566ed552e584745b7ce03a
mounikasreeram19 <134190279+mounikasreeram19@users.noreply.github.com>
2023-05-21 17:00:50 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit
+import snowflake.connector
+import requests
+import pandas as pd
+from urllib.error import URLError
+
 streamlit.title('My Mom''s New Healthy Diner')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
@@ -7,7 +12,7 @@ streamlit.text(' 🐔 Hard-Boiled Free-Rang Egg')
 streamlit.text('🥑🍞 Avacado Toast')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-import pandas as pd
+
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
@@ -17,7 +22,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 
 # write your own comment -what does the next line do? 
@@ -28,7 +33,7 @@ streamlit.dataframe(fruityvice_normalized)
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import snowflake.connector
+#import snowflake.connector
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 # my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
@@ -43,3 +48,10 @@ streamlit.write('The user entered ', choice)
 # allow the end user to add a fruit to the list
 add_my_fruit = my_cur.execute("insert into fruit_load_list values ('from streamlit') ")
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+# my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("The fruit load list contaions:")
+streamlit.dataframe(my_data_rows)
